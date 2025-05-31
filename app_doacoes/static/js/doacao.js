ca200718c8b70c33975dce61225fd8916bef5126
Miguel <miganjos88@gmail.com>
2025-05-31 11:33:42 -0300

    adicionando lógica de sugestões, ajustando o card no responsivo

--- app_doacoes/static/js/doacao.js
+++ app_doacoes/static/js/doacao.js
@@ -122,4 +122,10 @@ document.addEventListener("DOMContentLoaded", () => {
       };
     });
   }
+
+  const description = document.getElementsByClassName("card-text");
+  for (let i = 0; i < description.length; i++) {
+    if (description[i].innerHTML.length <= 95) continue;
+    description[i].innerHTML = description[i].innerHTML.substring(0, 95) + "..."
+  }
 });
